b59f193dc8d345983b791c8d412c8178d594bbb0
Rafael Figueiró <rafaelfigueiro@hotmail.com>
2023-12-13 08:51:51 -0300

    Primeiro commit: Projeto funcionando

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,20 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // A classe Nokia é uma subclasse da classe Smartphone, herdando suas propriedades e métodos.
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // O construtor da classe Nokia chama o construtor da classe base (Smartphone) usando a palavra-chave 'base'.
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            // O corpo do construtor está vazio, pois não há necessidade de realizar operações adicionais neste momento.
+        }
+
+        // Override do método InstalarAplicativo da classe base (Smartphone).
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
+using System;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Smartphone Nokia: ");
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1111111111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone iPhone: ");
+        Smartphone iphone = new Iphone(numero: "8955", modelo: "Modelo 2", imei: "2222222222", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");  // Corrigido para usar o objeto 'iphone'
+    }
+}
